df98ba30a4cbb24c8a9cb94f097d8469a2464aa3
jperezaguila <jperezaguila@yahoo.es>
2015-12-07 17:38:33 +0100

    Proyecto VideoTutorial

---
+++ WebApplicationFiltros/Filtros/FiltrosAcceso.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationFiltros.Utils;
+
+namespace WebApplicationFiltros.Filtros
+{
+    public class FiltrosAcceso : ActionFilterAttribute
+
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+           //string clientip = filterContext.HttpContext.Request.UserHostAddress;
+            string clientip = GetIP.GetIP4Address();
+            
+            if (!AddIp(clientip.Trim()))
+            {
+                filterContext.Result = new HttpStatusCodeResult(403);
+            }
+            base.OnActionExecuting(filterContext);
+         }
+
+        public bool AddIp(string clientip)
+        {
+            string[] clientipconvert = clientip.Trim().Split(new char[] { '.' });
+            string iplist = Convert.ToString(ConfigurationManager.AppSettings["AccessList"]);
+            string[] ipaccesslist = iplist.Trim().Split(new char[] { ',' });
+
+            foreach (var ipaccesslists in ipaccesslist)
+            {
+                if (ipaccesslists.Trim() == clientip)
+                {
+                    return true;
+                }
+
+                string[] validclientip = ipaccesslists.Trim().Split(new char[] { '.' });
+
+                bool result = true;
+
+                for (int i = 0; i < validclientip.Length; i++)
+                {
+                    if (validclientip[i] != "*")
+                    {
+                        if (validclientip[i] != clientipconvert[i])
+                        {
+                            result = false;
+                            break;
+                        }
+                    }
+                }
+                if (result)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ WebApplicationFiltros/Utils/GetIP.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+
+namespace WebApplicationFiltros.Utils
+{
+    public class GetIP
+    {
+        public static string GetIP4Address()
+        {
+            string IP4Address = String.Empty;
+
+            foreach (IPAddress IPA in Dns.GetHostAddresses(HttpContext.Current.Request.UserHostAddress))
+            {
+                if (IPA.AddressFamily.ToString() == "InterNetwork")
+                {
+                    IP4Address = IPA.ToString();
+                    break;
+                }
+            }
+
+            if (IP4Address != String.Empty)
+            {
+                return IP4Address;
+            }
+
+            foreach (IPAddress IPA in Dns.GetHostAddresses(Dns.GetHostName()))
+            {
+                if (IPA.AddressFamily.ToString() == "InterNetwork")
+                {
+                    IP4Address = IPA.ToString();
+                    break;
+                }
+            }
+
+            return IP4Address;
+        }
+    }
+
+
+}
